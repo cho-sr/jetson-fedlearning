32bab86dce7b120855e399d203ab689a4858a9e3
조석래 <cho020218@gmail.com>
2025-12-08 20:06:23 +0900

    Update client1.py

--- client1.py
+++ client1.py
@@ -19,7 +19,6 @@ from torchvision import models
 import torchvision.transforms.v2 as v2
 warnings.filterwarnings("ignore")
 
-
 SEED = 42
 random.seed(SEED)
 np.random.seed(SEED)
@@ -58,47 +57,47 @@ class Network1(nn.Module):
     def __init__(self, num_classes=4):
         super(Network1, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 20, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(20),
+            nn.Conv2d(3, 16, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(16),
             nn.ReLU6(inplace=True),
 
-            nn.Conv2d(20, 20, 3, padding=1, groups=20, bias=False),
-            nn.BatchNorm2d(20),
+            nn.Conv2d(16, 16, 3, padding=1, groups=16, bias=False),
+            nn.BatchNorm2d(16),
             nn.ReLU6(inplace=True),
-            nn.Conv2d(20, 30, 1, bias=False),
-            nn.BatchNorm2d(30),
+            nn.Conv2d(16, 32, 1, bias=False),
+            nn.BatchNorm2d(32),
             nn.ReLU6(inplace=True),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(30, 30, 3, padding=1, groups=30, bias=False),
-            nn.BatchNorm2d(30),
+            nn.Conv2d(32, 32, 3, padding=1, groups=32, bias=False),
+            nn.BatchNorm2d(32),
             nn.ReLU6(inplace=True),
-            nn.Conv2d(30, 45, 1, bias=False),
-            nn.BatchNorm2d(45),
-            nn.ReLU6(inplace=True),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(45, 45, 3, padding=1, groups=45, bias=False),
-            nn.BatchNorm2d(45),
-            nn.ReLU6(inplace=True),
-            nn.Conv2d(45, 67, 1, bias=False),
-            nn.BatchNorm2d(67),
+            nn.Conv2d(32, 64, 1, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU6(inplace=True),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(67, 67, 3, padding=1, groups=67, bias=False),
-            nn.BatchNorm2d(67),
+            nn.Conv2d(64, 64, 3, padding=1, groups=64, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU6(inplace=True),
-            nn.Conv2d(67, 100, 1, bias=False),
-            nn.BatchNorm2d(100),
+            nn.Conv2d(64, 128, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU6(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128, 128, 3, padding=1, groups=128, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(128, 256, 1, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU6(inplace=True),
 
             nn.AdaptiveAvgPool2d(1)
         )
         self.classifier = nn.Sequential(
-            nn.BatchNorm2d(100),
-            nn.Dropout(0.1),
-            nn.Conv2d(100, num_classes, kernel_size= 1)
+            nn.BatchNorm2d(256),
+            nn.Dropout(0.2),
+            nn.Conv2d(256, num_classes,kernel_size=1)
         )
 
     def forward(self, x):
@@ -143,7 +142,7 @@ def train(model, criterion, optimizer, train_loader):
 
         epoch_loss = running_loss / len(train_loader)
         epoch_accuracy = running_corrects.float() / total
-        print(f"Epoch [{epoch + 1}/{local_epochs}] => Train Loss: {epoch_loss:.4f} | Train Acc: {epoch_accuracy * 100:.2f}%")
+        print(f"Epoch [{epoch + 1}/{local_epochs}] => Train Loss: {epoch_loss:.4f} | Train Accuracy: {epoch_accuracy * 100:.2f}%")
 
     return model
 
@@ -179,9 +178,9 @@ def main():
     )
 
     model = Network1().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=1e-4)
     class_weights = torch.tensor([3.1, 4.0, 2.5, 3.3]).to(device)
-    criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
+    criterion = torch.nn.CrossEntropyLoss(weight=class_weights, label_smoothing=0.1)
 ##############################################################################################################################
 
 
